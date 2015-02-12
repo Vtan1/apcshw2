a2c7ca914a1041f19aa83206d61484ad1261d635
Victor Tan <victor@gmail.com>
2015-02-12 08:39:45 -0500

    .

---
+++ problems.java
@@ -0,0 +1,7 @@
+/* Knights Tour
+
+   given an n*n board, find a sequence for moves such that a knight can visit
+   every squareonce w/o repitition. moves like a knight in chess. (5x5)
+
+   -put 2 square thick border around the board (-1s) to prevent the knight from
+   jumping off.
