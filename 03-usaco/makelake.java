92623c83052d5292dcabf8deff855329e9041191
Vtan1 <victortan18@gmail.com>
2015-03-03 19:32:59 -0500

    .

---
+++ 03-usaco/makelake.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.util.*;
+
+public class MakeLake {
+    
+    private int R = 4;
+    private int C = 6;
+    private int E = 22;
+    private int N = 2;
+    private int[][] lake = new int[C][R];
+    private int[][] instructions = new int[N][3];
+
+    public MakeLake() {
+	int [] a = {28,25,20,32,34,36,27,25,20,20,30,34,24,20,20,20,20,30,20,20,14,14,20,20};
+	instructions[0] = {1,4,4};
+	instructions[1] = {1,1,10};
+	int i = 0;
+	for (int r=0;r<R;r++) {
+	    for (int c=0;c<C;c++) {
+		lake[c][r] = a[i];
+		i++;
+	    }
+	}
+    }
+
+    public String toString() {
+	String s = "";
+	for (int r=0;r<R;r++) {
+	    for (int c=0;c<C;c++) {
+		s = s + lake[c][r] + " ";
+	    }
+	    s = s + "\n";
+	}
+	return s;
+    }
+    
+    public void stomp() {
+	int x;
+	int y;
+	for (int i=0;i<N;i++) {
+	    x = instructions[N][0];
+	    y = instructions[N][1];
+	    
+	}
+    }
+    
+    
+    public static void main(String[] args) {
+	MakeLake m = new MakeLake();
+	System.out.println(m);
+    }
+}
--- 04-mergesort/mergesort.java
+++ 04-mergesort/mergesort.java
@@ -2,51 +2,56 @@ import java.io.*;
 import java.util.*;
 
 public class mergesort {
-    public ArrayList<Integer> test1;
-    public ArrayList<Integer> test2;
 
-    public mergesort() {
-	test1 = new ArrayList<Integer>();
-	test1.add(1);
-	test1.add(2);
-	test1.add(5);
-	test1.add(6);
-	test2 = new ArrayList<Integer>();
-	test2.add(3);
-	test2.add(4);
-	test2.add(6);
-	test2.add(8);
-    }
-
-    public ArrayList<Integer> getTest1() {
-	return test1;
-    }
-
-    public ArrayList<Integer> getTest2() {
-	return test2;
-    }
-    
     public ArrayList<Integer> merge (ArrayList<Integer> A, ArrayList<Integer> B) {
 	ArrayList<Integer> returnList = new ArrayList<Integer>();
 	while (A.size() > 0 && B.size() > 0) {
-	    //int i = 0;
 	    if (A.get(0) >= B.get(0)) {
-		returnList.add(A.remove(0));
-		//returnList.set(i,A.remove(0));
-		//A.remove(0);
-	    } else if (B.get(0) > A.get(0)) {
 		returnList.add(B.remove(0));
-		//returnList.set(i,B.remove(0));
-		//B.remove(0);
+	    } else {
+		returnList.add(A.remove(0));
 	    }
-	    //i++;
+	}
+	while (A.size() > 0) {
+	    returnList.add(A.remove(0));
+	} 
+	while (B.size() > 0) {
+	    returnList.add(B.remove(0));
 	}
 	return returnList;
     }
 
+    public ArrayList<Integer> sort (ArrayList<Integer> C) {
+	if (C.size() == 1) {
+	    return C;
+	}
+	ArrayList<Integer> half1 = new ArrayList<Integer>();
+	ArrayList<Integer> half2 = new ArrayList<Integer>();
+	int place;
+	for (place = 0; place < C.size()/2; place++) {
+	    half1.add(C.get(place));
+	}
+	while (place < C.size()) {
+	    half2.add(C.get(place));
+	    place++;
+	}
+	return merge(sort(half1),sort(half2));
+    }
+
     public static void main(String[] args) {
 	mergesort m = new mergesort();
-	System.out.println(m.merge(m.getTest2(),m.getTest1()));
-	//m.merge(test1,test2);
+	ArrayList<Integer> test1 = new ArrayList<Integer>();
+	ArrayList<Integer> test2 = new ArrayList<Integer>();
+	test1.add(1);
+	test1.add(5);
+	test1.add(2);
+	test1.add(6);
+	test1.add(4);
+	test2.add(3);
+	test2.add(4);
+	test2.add(6);
+	test2.add(8);
+	//System.out.println(m.merge(test1,test2));
+	//System.out.println(m.sort(test1));
     }
 }
