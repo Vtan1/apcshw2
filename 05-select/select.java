5f1a3c268c787bd2919d73cf2342bbdef8ffbf7b
Victor Tan <victor@gmail.com>
2015-03-05 08:38:57 -0500

    .

---
+++ 05-select/select.java
@@ -0,0 +1,8 @@
+/*
+1. choose a pivot value, which can be any value
+2. partition the list, rearrange so the values less than pivot are to the left
+and the larger ones are to the right
+3. now pivot is the at final location
+4. either we're done if pivot is kth smallest or call select on upper or lower
+parts as appropriate
+*/
