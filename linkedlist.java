6c8e2bbc30d3cbbed85bc34205bc7c68f2937e77
Victor Tan <victor@gmail.com>
2015-03-11 08:38:32 -0400

    .

---
+++ linkedlist.java
@@ -0,0 +1,6 @@
+/*
+  Linked list is comprised of nodes that contain some data and information on
+  how to get to the next node.
+  -set of nodes and a way to get to the "start" node
+  -can be circular for endless list.
+ */
