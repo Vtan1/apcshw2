de3d16bb592f5d4140c24415a0bf7d9247f12284
Victor Tan <victor@gmail.com>
2015-03-25 08:22:44 -0400

    .

---
+++ queue.java
@@ -0,0 +1,10 @@
+/*
+  FIFO - first in first out
+  Queue q = new Queue();
+  q.enqueue(x); //adds x to the back of the queue
+  x = q.dequeue(); //remove and return the item at the head if the queue
+  q.empty(); //is it empty?
+  q.front(); //returns front/head value
+
+
+ */
---
+++ stacks.java
@@ -0,0 +1,19 @@
+/*
+  Stacks
+  -take from the top
+  -place on the top
+  -supposed to restrict you to make things easier on the long run.
+  -Last in first out (LIFO)
+  
+  Implement this
+  _____________________________________________________________________________
+  myStack<Integer> s = new myStack<Integer>();
+  s.push(10); //pushes the 10 onto the top of the stack s
+  i=s.pop(); //remove the item from the top of the stack s and return it
+  s.empty(); //returns true or false
+  s.peek(); //returns but doesn't remove the top element of the stack
+  _____________________________________________________________________________
+  -push and pop are always needed for a stack class.
+  -usually have empty/isEmpty and can have peek
+  -sometimes have "cheat" methods like size and peeking at lower level elements
+ */
