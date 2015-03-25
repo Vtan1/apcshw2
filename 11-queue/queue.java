19038f4d7f0f7f805ad533cb01b7fc23d0a38808
Vtan1 <victortan18@gmail.com>
2015-03-25 19:34:56 -0400

    .

--- 11-queue/queue.java
+++ 11-queue/queue.java
@@ -1,22 +1,42 @@
-public class queue {
-    private Node<E> l;
-    private Node<E> f;
+public class queue<E> {
+    //back means back of the queue
+    private Node<E> back;
+    private Node<E> front;
     private int len;
-
+    
     public queue() {
-	l = new Node<E>();
-	f = new Node<E>();
-	l.setNext(f);
+	front = new Node<E>();
+	back = front;
 	len = 0;
     }
 
-    public void enqueue(x) {
-	Node<E> tmp = new Node<E>(x);
-	tmp.setNext(l);
-	l = tmp;
+    public void enqueue(E data) {
+	Node<E> tmp = new Node<E>(data);
+	back.setNext(tmp);
+	back = tmp;
 	len++;
     }
 
-    public Node<E> dequeue() {
-	
+    public E dequeue() {
+	E data = front.getData();
+	front = front.getNext();
+	len--;
+	return data;
+    }
+
+    public boolean empty() {
+	return (front == back);
+    }
+
+    public E front() {
+	return front.getNext().getData();
+    }
+
+    public static void main(String[] args) {
+	queue<Integer> q = new queue<Integer>();
+	q.enqueue(10);
+	q.enqueue(11);
+	//q.dequeue();
+	System.out.println(q.front());
+    }
 }
