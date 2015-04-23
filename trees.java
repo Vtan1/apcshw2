93d4bb7d6b48ceb79972f99ba1ce949f61088e2b
Victor Tan <victor@gmail.com>
2015-04-23 08:41:59 -0400

    .

--- trees.java
+++ trees.java
@@ -1,4 +1,6 @@
 /*
+
+4/22/15
   Trees
   -specification of a Graph
   -a Graph is a collection of nodes(vertices) and edges
@@ -19,4 +21,43 @@
   
   -binary trees ---> leaf or has one or two children
   -most similar to doublely linked list.
+
+4/23/15
+  -similar to doubley linked list in the computer, but visually different
+   because it points downwards instead of left and right.
+  -binary search tree is a binary tree such that given any node with value v,
+   all the nodes in the left subtree have values less than v and all the nodes
+   in the right subtree have values greater than 3.
+
+   public class Node {
+     private int data;
+     private Node left,right;
+     //get and set methods and constructors
+   }
+
+   Node search (Node T, int i) {
+     while (T!=null) {
+       int c = T.getData().compareTo(i);
+       if (c > 0) {
+         T=T.getRight();
+       } else if (c < 0) {
+         T=T.getLeft();
+       } else {
+         return T;
+       }
+     }
+     return null;
+   }
+
+   insert
+     if T==null
+        print T to the node you're inserting
+
+   insert as a new leaf
+     >create new node n
+     >search for the value of n, use a piggyback pointer so that you can go
+      backwards.
+     >when lead pointer (T) gets to null, piggyback (T2) will be at the node
+      before insertion point.
+     >insert new node to left or right of T2 as appropriate.
  */
