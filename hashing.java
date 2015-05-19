4fde86ed1c2c7fc36612c45bd3410a7b5d533b55
Victor Tan <victor@gmail.com>
2015-05-19 08:31:31 -0400

    .

--- hashing.java
+++ hashing.java
@@ -10,4 +10,22 @@
    -Solution:
     -hash table's array has to be sufficiently large (sparesely populated)
     -hash function should do a good job distributing items in the table.
- */
+
+5/19/15
+  -closed hashing
+   -everything is stored in the array
+   -collision: solutions?
+    -rehashing - use secondary hash
+    -linear probing - just shift the hashed thing to the next available slot.
+    -leave space in the table using a multiplier to store "collisions."
+  -insert function: expected O(1)
+  -retrieval: expected O(1)
+
+  -open hashing
+   -each array element is a pointer to a linked list of values that hash to that
+    element.
+   -insertion: O(1)
+    -in the linked list, add to the front of it to make it constant time.
+   -retrieval: expected O(1), O(n) with respect to the length (n) of linked list
+    at the current location.
+*/
